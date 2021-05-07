ef098bffe7fcacb95c55953e47d93c02e7eeb099
Ismail <ismail.desouki@gmail.com>
2021-05-07 08:46:33 -0700

    updated orderController

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,7 +2,7 @@ const orderRoutes = require('express').Router()
 const orderController = require('../controllers/orderController')
 
 orderRoutes.post('/create', orderController.create)
-orderRoutes.get('/',orderController.getAll)
+orderRoutes.get('/all',orderController.getAll)
 
 
 module.exports = orderRoutes
